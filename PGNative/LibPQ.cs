cdbdbea91d30df893c6176ca0763d5717f6950c4
Andrew Peters <anpete@microsoft.com>
2017-10-16 22:57:34 -0700

    Initial commit.

---
+++ PGNative/LibPQ.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable InconsistentNaming
+
+namespace PGNative
+{
+    public static class LibPQ
+    {
+        public enum PostgresPollingStatusType
+        {
+            PGRES_POLLING_FAILED = 0,
+            PGRES_POLLING_READING,
+            PGRES_POLLING_WRITING,
+            PGRES_POLLING_OK,
+            PGRES_POLLING_ACTIVE
+        }
+
+        public enum ExecStatusType
+        {
+            PGRES_EMPTY_QUERY = 0,
+            PGRES_COMMAND_OK,
+            PGRES_TUPLES_OK,
+            PGRES_COPY_OUT,
+            PGRES_COPY_IN,
+            PGRES_BAD_RESPONSE,
+            PGRES_NONFATAL_ERROR,
+            PGRES_FATAL_ERROR,
+            PGRES_COPY_BOTH,
+            PGRES_SINGLE_TUPLE
+        }
+
+        [DllImport("libpq", EntryPoint = "PQconnectStart")]
+        public static extern IntPtr PQconnectStart([In] [MarshalAs(UnmanagedType.LPStr)] string conninfo);
+
+        [DllImport("libpq", EntryPoint = "PQconnectPoll")]
+        public static extern PostgresPollingStatusType PQconnectPoll(IntPtr conn);
+
+        [DllImport("libpq", EntryPoint = "PQconnectdb")]
+        public static extern IntPtr PQconnectdb([In] [MarshalAs(UnmanagedType.LPStr)] string conninfo);
+
+        [DllImport("libpq", EntryPoint = "PQfinish")]
+        public static extern void PQfinish(IntPtr conn);
+
+        [DllImport("libpq", EntryPoint = "PQsendQuery")]
+        public static extern int PQsendQuery(IntPtr conn, [In] [MarshalAs(UnmanagedType.LPStr)] string query);
+
+        [DllImport("libpq", EntryPoint = "PQgetResult")]
+        public static extern IntPtr PQgetResult(IntPtr conn);
+
+        [DllImport("libpq", EntryPoint = "PQclear")]
+        public static extern void PQclear(IntPtr res);
+
+        [DllImport("libpq", EntryPoint = "PQresultStatus")]
+        public static extern ExecStatusType PQresultStatus(IntPtr res);
+
+        [DllImport("libpq", EntryPoint = "PQresStatus")]
+        public static extern IntPtr PQresStatus(ExecStatusType status);
+
+        [DllImport("libpq", EntryPoint = "PQgetvalue")]
+        public static extern IntPtr PQgetvalue(IntPtr res, int tup_num, int field_num);
+
+        [DllImport("libpq", EntryPoint = "PQerrorMessage")]
+        public static extern string PQerrorMessage(IntPtr conn);
+    }
+}
---
+++ PGNative/Program.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace PGNative
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            const string connInfo
+                = "host=localhost dbname=aspnet5-Benchmarks user=postgres password=Password1 client_encoding=utf8";
+
+            var conn = await Connect(connInfo);
+
+            Console.WriteLine("Connected.");
+
+            await Execute(conn);
+
+            Console.WriteLine("Executed query.");
+
+            LibPQ.PQfinish(conn);
+        }
+
+        private static async Task Execute(IntPtr conn)
+        {
+            if (LibPQ.PQsendQuery(conn, "select id, message from fortune") != 1)
+            {
+                throw new InvalidOperationException($"Unable to dispatch command: {LibPQ.PQerrorMessage(conn)}");
+            }
+
+            Console.WriteLine("Sent query.");
+
+            while (true)
+            {
+                var result = LibPQ.PQgetResult(conn);
+
+                if (result == IntPtr.Zero)
+                {
+                    break;
+                }
+
+                var status = LibPQ.PQresultStatus(result);
+                var statusName = Marshal.PtrToStringAnsi(LibPQ.PQresStatus(status));
+
+                Console.WriteLine($"Got result. ({statusName})");
+
+                switch (status)
+                {
+                    case LibPQ.ExecStatusType.PGRES_TUPLES_OK:
+                    {
+                        for (var i = 0; i < 12; i++)
+                        {
+                            var id = Marshal.PtrToStringUTF8(LibPQ.PQgetvalue(result, i, 0));
+                            var message = Marshal.PtrToStringUTF8(LibPQ.PQgetvalue(result, i, 1));
+
+                            Console.WriteLine($"Read row: [{id}, {message}]");
+                        }
+
+                        break;
+                    }
+                    case LibPQ.ExecStatusType.PGRES_EMPTY_QUERY:
+                    case LibPQ.ExecStatusType.PGRES_COMMAND_OK:
+                    case LibPQ.ExecStatusType.PGRES_COPY_OUT:
+                    case LibPQ.ExecStatusType.PGRES_COPY_IN:
+                    case LibPQ.ExecStatusType.PGRES_BAD_RESPONSE:
+                    case LibPQ.ExecStatusType.PGRES_NONFATAL_ERROR:
+                    case LibPQ.ExecStatusType.PGRES_FATAL_ERROR:
+                    case LibPQ.ExecStatusType.PGRES_COPY_BOTH:
+                    case LibPQ.ExecStatusType.PGRES_SINGLE_TUPLE:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                await Task.Yield();
+
+                LibPQ.PQclear(result);
+            }
+        }
+
+        private static async Task<IntPtr> Connect(string connInfo)
+        {
+            var conn = LibPQ.PQconnectStart(connInfo);
+
+            if (conn == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Unable to allocate connection handle.");
+            }
+
+            while (true)
+            {
+                switch (LibPQ.PQconnectPoll(conn))
+                {
+                    case LibPQ.PostgresPollingStatusType.PGRES_POLLING_OK:
+                        return conn;
+                    case LibPQ.PostgresPollingStatusType.PGRES_POLLING_FAILED:
+                        throw new InvalidOperationException($"Unable to connect: {LibPQ.PQerrorMessage(conn)}");
+                    case LibPQ.PostgresPollingStatusType.PGRES_POLLING_READING:
+                    case LibPQ.PostgresPollingStatusType.PGRES_POLLING_WRITING:
+                    case LibPQ.PostgresPollingStatusType.PGRES_POLLING_ACTIVE:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                await Task.Yield();
+            }
+        }
+    }
+}
